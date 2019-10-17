4c5404cb5bfa6669e73159e489784ac61a9c2a40
somaia khalil <sumaya_khair@live.com>
2019-10-17 23:53:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ new_hash {
+   :sumaya => "youngest",
+   :Mosaab => "oldest"
+ }
 end
 
 def pioneer
